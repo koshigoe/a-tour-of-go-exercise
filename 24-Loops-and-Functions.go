a8a6a78dd3cce5d60cf60930275921124a4f7be2
koshigoe <koshigoeb@gmail.com>
2015-03-08 17:07:26 +0900

    Implement `Newton's method`.

--- 24-Loops-and-Functions.go
+++ 24-Loops-and-Functions.go
@@ -1,12 +1,20 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"math"
 )
 
 func Sqrt(x float64) float64 {
+	z := x
+	for i := 0; i < 10; i++ {
+		z = z - (z*z-x)/(2*z)
+	}
+	return z
 }
 
 func main() {
-    fmt.Println(Sqrt(2))
+	for x := 1; x <= 15; x++ {
+		fmt.Printf("Sqrt(%d) = %f, math.Sqrt(%d) = %f\n", x, Sqrt(float64(x)), x, math.Sqrt(float64(x)))
+	}
 }
