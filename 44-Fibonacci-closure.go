5b879b0094f383b05971e2f41c842ace59b3a402
koshigoe <koshigoeb@gmail.com>
2015-03-10 21:28:21 +0900

    Implement `fibonacci`.

--- 44-Fibonacci-closure.go
+++ 44-Fibonacci-closure.go
@@ -5,11 +5,20 @@ import "fmt"
 // fibonacci is a function that returns
 // a function that returns an int.
 func fibonacci() func() int {
+	n, x, y := 0, 0, 1
+	return func() int {
+		if n == 0 {
+			n++
+		} else {
+			x, y = y, x+y
+		}
+		return x
+	}
 }
 
 func main() {
-    f := fibonacci()
-    for i := 0; i < 10; i++ {
-        fmt.Println(f())
-    }
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
 }
