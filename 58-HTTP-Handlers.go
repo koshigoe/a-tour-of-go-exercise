d7b478c2be7022fb0ed27014668b61824438a33b
koshigoe <koshigoeb@gmail.com>
2015-03-11 21:28:49 +0900

    Implement `ServeHTTP`.

--- 58-HTTP-Handlers.go
+++ 58-HTTP-Handlers.go
@@ -1,10 +1,32 @@
 package main
 
 import (
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
+type String string
+
+type Struct struct {
+	Greeting string
+	Punct    string
+	Who      string
+}
+
+func (h String) ServeHTTP(
+	w http.ResponseWriter,
+	r *http.Request) {
+	fmt.Fprint(w, h)
+}
+
+func (h Struct) ServeHTTP(
+	w http.ResponseWriter,
+	r *http.Request) {
+	fmt.Fprint(w, fmt.Sprintf("%s%s%s", h.Greeting, h.Punct, h.Who))
+}
+
 func main() {
-    // your http.Handle calls here
-    http.ListenAndServe("localhost:4000", nil)
+	http.Handle("/string", String("I'm a frayed knot."))
+	http.Handle("/struct", &Struct{"Hello", ":", "Gophers!"})
+	http.ListenAndServe("localhost:4000", nil)
 }
