382cbb7192a58dec9e35edbab0ccb45ba5af04a7
koshigoe <koshigoeb@gmail.com>
2015-03-13 23:36:45 +0900

    Implement `Reader`.

--- 61-Rot13-Reader.go
+++ 61-Rot13-Reader.go
@@ -1,18 +1,35 @@
 package main
 
 import (
-    "io"
-    "os"
-    "strings"
+	"io"
+	"os"
+	"strings"
 )
 
 type rot13Reader struct {
-    r io.Reader
+	r io.Reader
+}
+
+func (rot *rot13Reader) Read(p []byte) (n int, err error) {
+	n, err = rot.r.Read(p)
+
+	if err != nil {
+		return
+	}
+
+	for i := 0; i < len(p); i++ {
+		if (p[i] >= 'A' && p[i] <= 'M') || (p[i] >= 'a' && p[i] <= 'm') {
+			p[i] += 13
+		} else if (p[i] >= 'N' && p[i] <= 'Z') || (p[i] >= 'n' && p[i] <= 'z') {
+			p[i] -= 13
+		}
+	}
+	return
 }
 
 func main() {
-    s := strings.NewReader(
-        "Lbh penpxrq gur pbqr!")
-    r := rot13Reader{s}
-    io.Copy(os.Stdout, &r)
+	s := strings.NewReader(
+		"Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	io.Copy(os.Stdout, &r)
 }
